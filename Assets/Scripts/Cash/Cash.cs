d840c3781920e2231756c37a18ae761bfcfe6f49
Мирон <miron.developer@gmail.com>
2022-05-16 20:57:22 +0300

    Solved the bug in script "Cash"
    testing in progress now

--- Assets/Scripts/Cash/Cash.cs
+++ Assets/Scripts/Cash/Cash.cs
@@ -10,10 +10,11 @@ public class Cash : MonoBehaviour
     [SerializeField] private List<Transform> _cashPositions = new();
     [Min(3)] [SerializeField] private int _productCost = 3;
 
-    private Dictionary<GameObject, bool> _cash = new();
+    private List<GameObject> _cash = new();
     private Player _player;
     private int _cashAmount;
     private int _cashPosition;
+    private int _maxMoneyHeight = 8;
 
     public int ProductCost => _productCost;
 
@@ -41,6 +42,11 @@ public class Cash : MonoBehaviour
         {
             if (_cashPosition >= _cashPositions.Count)
             {
+                if ((_cashAmount / _productCost / _cashPositions.Count) >= _maxMoneyHeight)
+                {
+                    continue;
+                }
+                
                 _cashPosition = 0;
                 _cashPositions.ForEach(x => x.position += Vector3.up * 0.25f);
             }
@@ -57,30 +63,30 @@ public class Cash : MonoBehaviour
             {
                 cashObj = Instantiate(_cashPrefab, playerPosition.position, Quaternion.identity);
                 cashObj.transform.SetParent(transform);
-                _cash.Add(cashObj, false);
+                _cash.Add(cashObj);
 
                 Debug.Log("Cash is Instantiated");
             }
 
-            cashObj.transform.DOMove(_cashPositions[_cashPosition].position, 0.3f).OnComplete(() =>
+            if (!_player)
             {
-                _cash[cashObj] = true;
-            });
+                cashObj.transform.DOMove(_cashPositions[_cashPosition].position, 0.3f);
+            }
+            else
+            {
+                WithdrawCash(_player, cashObj);
+            }
             
             _cashPosition++;
         }
 
         _cashAmount += cash;
-/*        if (_player)
-        {
-            StartCoroutine(WithdrawCash(_player));
-        }*/
     }
 
     public IEnumerator WithdrawCash(Player player)
     {
         float duration = 0.08f;
-        foreach (var item in _cash.Reverse())
+        for (int i = _cash.Count - 1; i >= 0; i--)
         {
             if ((_cashAmount == 0) || (!_player))
             {
@@ -88,20 +94,16 @@ public class Cash : MonoBehaviour
                 yield break;
             }
 
-            if (!item.Key.activeInHierarchy || !item.Value)
+            if (!_cash[i].activeInHierarchy)
             {
                 Debug.Log("Cash is not active");
                 continue;
             }
 
-            item.Key.transform.DOMove(new Vector3(player.transform.position.x, player.transform.position.y / 2, player.transform.position.z), duration).OnComplete(() =>
+            _cash[i].transform.DOMove(new Vector3(player.transform.position.x, player.transform.position.y / 2, player.transform.position.z), duration).OnComplete(() =>
             {
-                item.Key.SetActive(false);
+                _cash[i].SetActive(false);
             });
-
-            player.CashData.AddCash(_productCost);
-            _cashAmount -= _productCost;
-            _cashPosition--;
 
             if (_cashPosition + 1 % _cashPositions.Count == 0)
             {
@@ -111,47 +113,28 @@ public class Cash : MonoBehaviour
             Debug.Log("Cash is withdrawn");
             yield return new WaitForSeconds(duration);
         }
-/*            
-        for (int i = _cash.Count - 1; i >= 0; i--)
-        {
-            if((_cashAmount == 0) || (!_player))
-            {
-                Debug.Log("Cash is Empty");
-                yield break;
-            }
-
-            if (!_cash[])
-            {
-                Debug.Log("Cash is not active");
-                continue;
-            }
-            
-            _cash[i].transform.DOMove(new Vector3(player.transform.position.x, player.transform.position.y / 2, player.transform.position.z), duration).OnComplete(() =>
-            {
-                _cash[i].SetActive(false);
-            });
-
-            player.CashData.AddCash(_productCost);
-            _cashAmount -= _productCost;
-            _cashPosition--;
-
-            if(_cashPosition + 1 % _cashPositions.Count == 0)
-            {
-                _cashPositions.ForEach(x => x.position -= Vector3.up * 0.25f);
-            }
-
-            Debug.Log("Cash is withdrawn");
-            yield return new WaitForSeconds(duration);
-        }*/
 
         _cashPositions.ForEach(cashPosition => cashPosition.localPosition = new Vector3(cashPosition.localPosition.x, 0, cashPosition.localPosition.z));
         _cashAmount = 0;
         _cashPosition = 0;
     }
 
+    public void WithdrawCash(Player player, GameObject cash)
+    {
+        cash.transform.DOMove(_player.transform.position + new Vector3(0, 0.7f, 0), 0.3f).OnComplete(() =>
+        {
+            _cashAmount -= _productCost;
+            _cashPosition--;
+            
+            player.CashData.AddCash(_productCost);
+            cash.SetActive(false);
+
+        });
+    }
+
     private GameObject GetPooledObject()
     {
-        foreach(GameObject poolObject in _cash.Keys)
+        foreach(GameObject poolObject in _cash)
         {
             if (!poolObject.activeInHierarchy)
             {
